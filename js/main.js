db8019fad2ec3da5320d7347449dcaa8ef36e8fe
EmilAbsalyamov <absalyamov.work@gmail.com>
2021-02-17 00:53:49 +0300

    burger button 2.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-document.getElementById('menu_btn').onclick = function() {
-   document.getElementById('burgerJS').classList.toggle('burger-menu_active');
+const btnId = document.getElementById('burgerJS')
+btnId.onclick = () => btnId.classList.toggle('burger-menu_active')
+
+let arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+for (i = arr.length; i < 13; i++) {
+   let newVal = arr[i-1] + arr[i - 2]
+   arr.push(newVal)
+   console.log(arr);
 }
+
